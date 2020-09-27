5a09bd999cc4147bfd6768918bb36e0a36644882
Nick Volpe <navolpe@gmail.com>
2020-09-27 15:03:59 -0700

    commit small changes

--- tv/MapApp/App.tsx
+++ tv/MapApp/App.tsx
@@ -9,93 +9,69 @@
  */
 
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
-
-import {
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, Text, View} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
 
 declare const global: {HermesInternal: null | {}};
 
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoibmF2b2xwZSIsImEiOiJja2ZrZ2JtcmEwdmcwMndtam5kc3A4MG1kIn0.D4Z19FKgr3p_8s2zEybx0Q',
+);
+
 const App = () => {
   return (
-    <>
-      <View style={styles.body}>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Step One</Text>
-          <Text style={styles.sectionDescription}>
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Text>
-        </View>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>See Your Changes</Text>
-          <Text style={styles.sectionDescription}>
-            <ReloadInstructions />
-          </Text>
-        </View>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Debug</Text>
-          <Text style={styles.sectionDescription}>
-            <DebugInstructions />
-          </Text>
-        </View>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Learn More</Text>
-          <Text style={styles.sectionDescription}>
-            Read the docs to discover what to do next:
-          </Text>
-        </View>
-        <LearnMoreLinks />
-      </View>
-    </>
+    <Text>See Your Changes</Text>
+    // <View style={styles.matchParent}>
+    //   <MapboxGL.MapView
+    //     styleURL={MapboxGL.StyleURL.Dark}
+    //     style={styles.matchParent}>
+    //     <MapboxGL.Camera followZoomLevel={12} />
+    //   </MapboxGL.MapView>
+    // </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  matchParent: {
+    flex: 1,
+    backgroundColor: 'blue',
   },
+  // scrollView: {
+  //   backgroundColor: Colors.lighter,
+  // },
   engine: {
     position: 'absolute',
     right: 0,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
+  // body: {
+  //   backgroundColor: Colors.white,
+  // },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  //   color: Colors.black,
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  //   color: Colors.dark,
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
 });
 
 export default App;
