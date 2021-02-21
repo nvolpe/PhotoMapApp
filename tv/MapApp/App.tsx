98043fe874131df0cf779b8a18dfd7b3e5c2cdff
Nick Volpe <navolpe@gmail.com>
2021-02-20 22:05:21 -0800

    added react-navigation to project.
    likely need to add forked version of rn-screens

--- tv/MapApp/App.tsx
+++ tv/MapApp/App.tsx
@@ -7,125 +7,46 @@
  */
 
 import React from 'react';
-import {useState, useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import CameraRoll from '@react-native-community/cameraroll';
-import {LatLng} from 'react-native-maps';
+// import {useState, useEffect} from 'react';
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   View,
+//   Text,
+//   TouchableOpacity,
+// } from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View, Text} from 'react-native';
 
-import {ImageData} from './src/testData/ImageData';
-
-import AppleMap from './src/components/map/AppleMap';
-import ImageView from './src/components/image/ImageView';
-
-const useIcloudPhotos = false;
-
-interface PhotoAlbum {
-  photos: CameraRoll.PhotoIdentifiersPage | null;
-}
-
-function App() {
-  const [photos, setPhotos] = useState<PhotoAlbum>({
-    photos: null,
-  });
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-  const [
-    currentPhoto,
-    setCurrentPhoto,
-  ] = useState<CameraRoll.PhotoIdentifier | null>(null);
-
-  const [latLng, setLatLng] = useState<LatLng | null>(null);
-
-  const requestPhotos = async () => {
-    console.log('requestPhotos');
-    const params = {
-      first: 20,
-    };
-
-    if (useIcloudPhotos) {
-      const output: CameraRoll.PhotoIdentifiersPage = await CameraRoll.getPhotos(
-        params,
-      );
-      setPhotos({photos: output});
-      console.log('Got Photos', output);
-    } else {
-      let testPhotos: PhotoAlbum = {
-        photos: ImageData,
-      };
-      console.log('Got Photos', testPhotos.photos?.edges[1]);
-      setPhotos({photos: testPhotos.photos});
-    }
-  };
-
-  useEffect(() => {
-    async function getPhotos() {
-      await requestPhotos();
-    }
-    getPhotos();
-  }, []);
-
-  const handleButtonPress = async () => {
-    if (photos.photos?.edges[currentIndex] && currentIndex < 7) {
-      let photo = photos.photos?.edges[currentIndex];
-      setCurrentPhoto(photo);
-      updateMap(photo);
-    }
-  };
-
-  const updateMap = (photo: CameraRoll.PhotoIdentifier | null) => {
-    if (
-      photo &&
-      photo.node.location?.latitude &&
-      photo.node.location?.longitude
-    ) {
-      setLatLng({
-        latitude: photo.node.location.latitude,
-        longitude: photo.node.location.longitude,
-      });
-    }
-    setCurrentIndex(currentIndex + 1);
-  };
-
+function HomeScreen() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <AppleMap latLng={latLng} />
-        <TouchableOpacity
-          hasTVPreferredFocus={true}
-          style={styles.testButton}
-          onPress={handleButtonPress}>
-          <Text style={styles.sectionTitle}>Get Photos</Text>
-        </TouchableOpacity>
-        <ImageView photo={currentPhoto} />
-      </View>
-    </SafeAreaView>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen Test</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-  },
-  testButton: {
-    // flex: 1,
-    width: 100,
-    height: 100,
-    borderColor: 'yellow',
-    backgroundColor: 'purple',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-});
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
