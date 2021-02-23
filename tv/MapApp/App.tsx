652ec37de91b0f7a39d0b1c4121bbffa20edd098
Nick Volpe <navolpe@gmail.com>
2021-02-22 21:58:07 -0800

    sample navigation code

--- tv/MapApp/App.tsx
+++ tv/MapApp/App.tsx
@@ -8,30 +8,53 @@
 
 import React from 'react';
 // import {useState, useEffect} from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   View,
-//   Text,
-//   TouchableOpacity,
-// } from 'react-native';
+import {SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {View, Text} from 'react-native';
+import MapScreen from './src/screens/mapScreen/MapScreen';
 
-function HomeScreen() {
+function HomeScreen({navigation}: any) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen Test</Text>
+      <TouchableOpacity
+        // hasTVPreferredFocus={true}
+        style={styles.testButton}
+        onBlur={() => {
+          console.log('Home on blur');
+        }}
+        onFocus={() => {
+          console.log('Home on focus');
+        }}
+        onPress={() => {
+          navigation.navigate('Details');
+        }}>
+        <Text>Nav test</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({navigation}: any) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <TouchableOpacity
+        // hasTVPreferredFocus={true}
+        style={styles.testButton}
+        onBlur={() => {
+          console.log('Details on blur');
+        }}
+        onFocus={() => {
+          console.log('Details on focus');
+        }}
+        onPress={() => {
+          navigation.navigate('Map');
+        }}>
+        <Text>Nav test</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -41,12 +64,28 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  testButton: {
+    width: 100,
+    height: 100,
+    borderColor: 'yellow',
+    backgroundColor: 'purple',
+  },
+});
 
 export default App;
--- tv/MapApp/src/screens/mapScreen/MapScreen.tsx
+++ tv/MapApp/src/screens/mapScreen/MapScreen.tsx
@@ -1,131 +1,131 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-// import React from 'react';
-// import {useState, useEffect} from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   View,
-//   Text,
-//   TouchableOpacity,
-// } from 'react-native';
-// import {Colors} from 'react-native/Libraries/NewAppScreen';
-// import CameraRoll from '@react-native-community/cameraroll';
-// import {LatLng} from 'react-native-maps';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import CameraRoll from '@react-native-community/cameraroll';
+import {LatLng} from 'react-native-maps';
 
-// import {ImageData} from './src/testData/ImageData';
+import {ImageData} from '../../testData/ImageData';
 
-// import AppleMap from './src/components/map/AppleMap';
-// import ImageView from './src/components/image/ImageView';
+import AppleMap from '../../components/map/AppleMap';
+import ImageView from '../../components/image/ImageView';
 
-// const useIcloudPhotos = false;
+const useIcloudPhotos = false;
 
-// interface PhotoAlbum {
-//   photos: CameraRoll.PhotoIdentifiersPage | null;
-// }
+interface PhotoAlbum {
+  photos: CameraRoll.PhotoIdentifiersPage | null;
+}
 
-// function MapScreen() {
-//   const [photos, setPhotos] = useState<PhotoAlbum>({
-//     photos: null,
-//   });
-//   const [currentIndex, setCurrentIndex] = useState<number>(0);
+function MapScreen() {
+  const [photos, setPhotos] = useState<PhotoAlbum>({
+    photos: null,
+  });
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-//   const [
-//     currentPhoto,
-//     setCurrentPhoto,
-//   ] = useState<CameraRoll.PhotoIdentifier | null>(null);
+  const [
+    currentPhoto,
+    setCurrentPhoto,
+  ] = useState<CameraRoll.PhotoIdentifier | null>(null);
 
-//   const [latLng, setLatLng] = useState<LatLng | null>(null);
+  const [latLng, setLatLng] = useState<LatLng | null>(null);
 
-//   const requestPhotos = async () => {
-//     console.log('requestPhotos');
-//     const params = {
-//       first: 20,
-//     };
+  const requestPhotos = async () => {
+    console.log('requestPhotos');
+    const params = {
+      first: 20,
+    };
 
-//     if (useIcloudPhotos) {
-//       const output: CameraRoll.PhotoIdentifiersPage = await CameraRoll.getPhotos(
-//         params,
-//       );
-//       setPhotos({photos: output});
-//       console.log('Got Photos', output);
-//     } else {
-//       let testPhotos: PhotoAlbum = {
-//         photos: ImageData,
-//       };
-//       console.log('Got Photos', testPhotos.photos?.edges[1]);
-//       setPhotos({photos: testPhotos.photos});
-//     }
-//   };
+    if (useIcloudPhotos) {
+      const output: CameraRoll.PhotoIdentifiersPage = await CameraRoll.getPhotos(
+        params,
+      );
+      setPhotos({photos: output});
+      console.log('Got Photos', output);
+    } else {
+      let testPhotos: PhotoAlbum = {
+        photos: ImageData,
+      };
+      console.log('Got Photos', testPhotos.photos?.edges[1]);
+      setPhotos({photos: testPhotos.photos});
+    }
+  };
 
-//   useEffect(() => {
-//     async function getPhotos() {
-//       await requestPhotos();
-//     }
-//     getPhotos();
-//   }, []);
+  useEffect(() => {
+    async function getPhotos() {
+      await requestPhotos();
+    }
+    getPhotos();
+  }, []);
 
-//   const handleButtonPress = async () => {
-//     if (photos.photos?.edges[currentIndex] && currentIndex < 7) {
-//       let photo = photos.photos?.edges[currentIndex];
-//       setCurrentPhoto(photo);
-//       updateMap(photo);
-//     }
-//   };
+  const handleButtonPress = async () => {
+    if (photos.photos?.edges[currentIndex] && currentIndex < 7) {
+      let photo = photos.photos?.edges[currentIndex];
+      setCurrentPhoto(photo);
+      updateMap(photo);
+    }
+  };
 
-//   const updateMap = (photo: CameraRoll.PhotoIdentifier | null) => {
-//     if (
-//       photo &&
-//       photo.node.location?.latitude &&
-//       photo.node.location?.longitude
-//     ) {
-//       setLatLng({
-//         latitude: photo.node.location.latitude,
-//         longitude: photo.node.location.longitude,
-//       });
-//     }
-//     setCurrentIndex(currentIndex + 1);
-//   };
+  const updateMap = (photo: CameraRoll.PhotoIdentifier | null) => {
+    if (
+      photo &&
+      photo.node.location?.latitude &&
+      photo.node.location?.longitude
+    ) {
+      setLatLng({
+        latitude: photo.node.location.latitude,
+        longitude: photo.node.location.longitude,
+      });
+    }
+    setCurrentIndex(currentIndex + 1);
+  };
 
-//   return (
-//     <SafeAreaView>
-//       <View style={styles.container}>
-//         <AppleMap latLng={latLng} />
-//         <TouchableOpacity
-//           hasTVPreferredFocus={true}
-//           style={styles.testButton}
-//           onPress={handleButtonPress}>
-//           <Text style={styles.sectionTitle}>Get Photos</Text>
-//         </TouchableOpacity>
-//         <ImageView photo={currentPhoto} />
-//       </View>
-//     </SafeAreaView>
-//   );
-// }
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <AppleMap latLng={latLng} />
+        <TouchableOpacity
+          hasTVPreferredFocus={true}
+          style={styles.testButton}
+          onPress={handleButtonPress}>
+          <Text style={styles.sectionTitle}>Get Photos</Text>
+        </TouchableOpacity>
+        <ImageView photo={currentPhoto} />
+      </View>
+    </SafeAreaView>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     width: '100%',
-//     height: '100%',
-//   },
-//   testButton: {
-//     // flex: 1,
-//     width: 100,
-//     height: 100,
-//     borderColor: 'yellow',
-//     backgroundColor: 'purple',
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  testButton: {
+    // flex: 1,
+    width: 100,
+    height: 100,
+    borderColor: 'yellow',
+    backgroundColor: 'purple',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+});
 
-// export default MapScreen;
+export default MapScreen;
